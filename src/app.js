2ef08f10dce7a67da7779ffccbc33b4e0f771737
Aditya Tiwari <tiwariaditya973@gmail.com>
2023-01-07 15:30:09 +0530

    commit 3

--- src/app.js
+++ src/app.js
@@ -1,2 +1,40 @@
-const {v4 : uuidv4} = require('uuid');
-console.log(uuidv4);
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const PORT = 3000;
+const customers = [
+    {
+        "Name" : "Aditya",
+        "industry" : "it"
+    },{
+        "Name" : "John",
+        "industry" : "music"
+    },{
+        "Name" : "Sameer",
+        "industry" : "Fax",
+    }
+]
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome");
+});
+app.get('/api/customers',(req,res)=>{
+    res.send({"customers" : customers});
+});
+
+app.post('/',(req,res)=>{
+    res.send("This is a post request!");
+});
+
+app.post('/api/customers',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.listen(PORT,()=>{
+    console.log("App listening to port " + PORT);
+})
