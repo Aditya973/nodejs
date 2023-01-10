faa2b626d98d253ea0dad4ee02aa457468649aba
Aditya Tiwari <tiwariaditya973@gmail.com>
2023-01-10 21:05:42 +0530

    3 commit ig

--- src/app.js
+++ src/app.js
@@ -1,29 +1,53 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
+const Customer = require('./models/customer'); // '.' means to from 'this directory(src)' to models/customer
+
+mongoose.set('strictQuery',false);
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-const PORT = 3000;
+if(process.env.NODE_ENV !== 'production'){
+    dotenv.config();
+}
+const CONNECTION = process.env.CONNECTION;
+const PORT = process.env.PORT || 3000;
 const customers = [
     {
         "Name" : "Aditya",
         "industry" : "it"
-    },{
+    },
+    {
         "Name" : "John",
         "industry" : "music"
-    },{
+    },
+    {
         "Name" : "Sameer",
         "industry" : "Fax",
     }
-]
+];
 
+const customer = new Customer({
+    name : "surya",
+    industry : "finance"
+});
+
+// customer.save();   /*save() save the collection in mongodb
 
 app.get('/',(req,res)=>{
-    res.send("Welcome");
+    res.send('Welcome!');
 });
-app.get('/api/customers',(req,res)=>{
-    res.send({"customers" : customers});
+app.get('/api/customers',async (req,res)=>{
+    // console.log(await mongoose.connection.db.listCollections().toArray()); /*output the metadata of database in console
+    try{
+        const result = await Customer.find(); // find() grab everthing from Customer Schema
+        res.send({"customers" : result}); //res.json({"customers" : result});
+    }
+    catch(e){
+        res.status(500).json({error:e.message});
+    }
 });
 
 app.post('/',(req,res)=>{
@@ -35,6 +59,16 @@ app.post('/api/customers',(req,res)=>{
     res.send(req.body);
 });
 
-app.listen(PORT,()=>{
-    console.log("App listening to port " + PORT);
-})
+
+const start = async() =>{
+    try{
+        await mongoose.connect(CONNECTION);
+        app.listen(PORT,()=>{
+            console.log("App listening to port " + PORT);
+        });
+    }
+    catch(e){
+        console.log(e.message);
+    }
+};
+start();
---
+++ src/models/customer.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+    name : String,
+    industry : String
+});
+
+module.exports = mongoose.model('Client',customerSchema); // model(name of collection in mongodb, schema)
+// mongo db pluralizes the name of the collection and set the first letter to lowercase(ex: Client -> clients)
+// exports -> means what things are we going to export from the customer.js file when we import them or require them on other file like app.js
